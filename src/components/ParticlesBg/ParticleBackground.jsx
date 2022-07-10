b5d31aa44156be9d8d5e38eda090bccf87cce73e
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-07-10 17:12:17 -0300

    Delete src directory

--- src/components/ParticlesBg/ParticleBackground.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Particles from "react-tsparticles";
-import ParticleConfig from "./particle-config"
-
-export default function ParticleBackground() {
-    return (
-        <Particles className="particles-css" params={ParticleConfig}></Particles>
-    );
-}
